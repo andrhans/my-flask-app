797daeb55042bc6e0759434441b9a47bb845f168
andrhans <andreahm@gmx.com>
2020-12-08 16:02:55 +0000

    Login and register missing database link

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,28 @@
 
-
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}'.format(
+    username='andrhans',
+    password='VSVw6ni1mzFAOc5M90GY',
+    hostname='andrhans.mysql.pythonanywhere-services.com',
+    databasename='andrhans$car-app',
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+app.secret_key = 'c8skRkkMostrhqioIWlC785ZnJEcvuFS'
 
 @app.route('/')
-def index():
-    return 'This is the first page!'
+def login():
+    return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
